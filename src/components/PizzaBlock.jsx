044da6512deb7235891ea36e32acc9164ff6f694
allexib <ooomegasib@gmail.com>
2022-05-30 19:53:28 +0300

    popup, sort

--- src/components/PizzaBlock.jsx
+++ src/components/PizzaBlock.jsx
@@ -17,14 +17,24 @@ function PizzaBlock({ title, price, imageUrl, sizes, types }) {
       <div className="pizza-block__selector">
         <ul>
           {types.map((typeId) => (
-            <li key={typeId} onClick={()=>setActiveType(typeId)} className={activeType === typeId ? "active" : ""}>
+            <li
+              key={typeId}
+              onClick={() => setActiveType(typeId)}
+              className={activeType === typeId ? "active" : ""}
+            >
               {typeNames[typeId]}
             </li>
           ))}
         </ul>
         <ul>
-          {sizes.map((size,i) => (
-            <li key={size} onClick={()=>setActiveSize(i)} className={activeSize === i ? "active" : ""}>{size} см.</li>
+          {sizes.map((size, i) => (
+            <li
+              key={size}
+              onClick={() => setActiveSize(i)}
+              className={activeSize === i ? "active" : ""}
+            >
+              {size} см.
+            </li>
           ))}
         </ul>
       </div>
