6cd75730faae034ff1db0cc2864bb0508a0a66e9
allexib <ooomegasib@gmail.com>
2022-06-02 12:20:36 +0300

    skeleton

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import React from "react";
 import Header from "./components/Header";
 import Categories from "./components/Categories";
 import Sort from "./components/Sort";
-import PizzaBlock from "./components/PizzaBlock";
+import PizzaBlock from "./components/PizzaBlock/PizzaBlock";
+import Skeleton from "./components/PizzaBlock/Skeleton";
 
 import "./scss/app.scss";
 
 function App() {
   const [items, setItems] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
 
   //https://629636a1810c00c1cb718672.mockapi.io/items
   React.useEffect(() => {
@@ -16,6 +18,7 @@ function App() {
       .then((res) => res.json())
       .then((arr) => {
         setItems(arr);
+        setIsLoading(false)
       });
   }, []);
 
@@ -30,9 +33,9 @@ function App() {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            {items.map((obj) => (
-              <PizzaBlock key={obj.id} {...obj} />
-            ))}
+            {isLoading
+              ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
+              : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
           </div>
         </div>
       </div>
